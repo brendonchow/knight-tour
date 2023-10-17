647192e2b7e7c81cf1634ca65dc72f07ca406206
Brendon Chow <brendonchow123@gmail.com>
2023-10-18 08:12:18 +1100

    Add comments

--- script.js
+++ script.js
@@ -95,33 +95,27 @@ const getGraphEdges = (graph) => {
   return [lowestDegree, lowestDegreeCount, totalEdges];
 };
 
-// Maybe Implement: If one reachable vertex has a degree of one, move to it
-// let lowestSize = Infinity;
 const knightMoves = (pos, graph = initializeGraph(), moves = []) => {
-  // if (graph.size < lowestSize) {
-  //   lowestSize = graph.size;
-  //   console.log(lowestSize);
-  // }
-
   const edges = removeNode(pos, graph);
   moves.push(pos);
 
   if (graph.size === 0) {
-    return [graph, moves];
+    return moves;
   }
-  if (edges.size === 0) null;
+
+  if (edges.size === 0) return null;
 
   let lowestDegree = 0;
   let lowestDegreeCount = Infinity;
   let totalEdges = 0;
   const bestMoves = new Set();
-  // Why is depth 0 so slow??
-  const depth = 0;
+  // Why does depth 1 only require 63 recursive knightMoves calls while depth 0 takes
+  // forever. Seems like depth 1 never searches the wrong path.
+  // Why does depth 2 not produce the correct result?
   for (const edge of edges) {
     const [minDegree, minDegreeCount, countEdges] = getOptimalMoves(
       edge,
-      graph
-      // depth
+      graph,
     );
     if (
       minDegree == Infinity &&
@@ -133,12 +127,11 @@ const knightMoves = (pos, graph = initializeGraph(), moves = []) => {
       break;
     }
     if (
-      minDegree > 0 &&
-      (minDegree > lowestDegree ||
-        (minDegree === lowestDegree && minDegreeCount < lowestDegreeCount) ||
-        (minDegree === lowestDegree &&
-          minDegreeCount === lowestDegreeCount &&
-          countEdges > totalEdges))
+      minDegree > lowestDegree ||
+      (minDegree === lowestDegree && minDegreeCount < lowestDegreeCount) ||
+      (minDegree === lowestDegree &&
+        minDegreeCount === lowestDegreeCount &&
+        countEdges > totalEdges)
     ) {
       lowestDegree = minDegree;
       lowestDegreeCount = minDegreeCount;
@@ -151,12 +144,15 @@ const knightMoves = (pos, graph = initializeGraph(), moves = []) => {
       countEdges === totalEdges
     ) {
       bestMoves.add(edge);
-    }
+    } 
   }
+
   for (const move of bestMoves) {
-    const graphClone = lodash.cloneDeep(graph);
-    const movesClone = lodash.cloneDeep(moves);
-    const result = knightMoves(move, graphClone, movesClone);
+    const result = knightMoves(
+      move,
+      lodash.cloneDeep(graph),
+      lodash.cloneDeep(moves)
+    );
     if (result) return result;
   }
 };
@@ -209,13 +205,16 @@ const getOptimalMoves = (pos, graph, depth = 1) => {
   return [lowestDegree, lowestDegreeCount, totalEdges];
 };
 
+// TESTS
+let count = 0;
 const test = () => {
   let max = 0;
-  for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < 7; j++) {
+  for (let i = 0; i <= 7; i++) {
+    for (let j = 0; j <= 7; j++) {
       const start = performance.now();
       const result = knightMoves(`${i}${j}`);
       if (result) {
+        count++;
         const end = performance.now();
         const difference = end - start;
         if (difference > max) max = difference;
@@ -226,16 +225,17 @@ const test = () => {
     }
   }
   console.log(`Max time: ${max} ms`);
+  console.log(`Count: ${count}`);
 };
-
 test();
+
 // const start = performance.now();
 // const result = knightMoves("33");
 // if (result) {
 //   const end = performance.now();
-//   console.log(result[0]);
-//   displayBoard(result[1]);
+//   console.log(result);
+//   displayBoard(result);
 //   console.log(`Execution time: ${end - start} ms`);
 // } else {
-//   ("Error!");
+//   console.log("Error!");
 // }
