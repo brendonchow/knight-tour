0639359bfcde24f15c3c28390d200faf53b7a723
Brendon Chow <brendonchow123@gmail.com>
2023-11-16 10:53:19 +1100

    Add width height and alt for knight img and remove roboto font

--- src/display.js
+++ src/display.js
@@ -11,8 +11,12 @@ moveAudio.muted = true;
 moveAudio.src = moveSound;
 const volumeOn = new Image();
 volumeOn.src = vOn;
+
 const knight = new Image();
 knight.src = knightIcon;
+knight.alt = "Knight Icon";
+knight.style.width = "90px";
+knight.style.height = "90px";
 knight.addEventListener("click", (event) => event.stopPropagation());
 knight.draggable = true;
 
